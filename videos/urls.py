5ad0262321f4ca08ea7de84ab742d95b1fc2c031
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-04-23 21:10:19 +0600

    added func 'search video by test

--- videos/urls.py
+++ videos/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from . import views as v
-from .views import AddUserToWatchedVideo
+from .views import AddUserToWatchedVideo, VideoByTestIdAPIView
 
 urlpatterns = [
     path('video/create', v.VideoCreateAPIView.as_view()),
     path('video/all', v.VideoListAPIView.as_view()),
     path('video/<int:pk>', v.VideoDetailAPIView.as_view()),
     path('add-user/<int:video_id>', AddUserToWatchedVideo.as_view(), name='add_user_to_watched_video'),
+    path('video/<int:test_id>/', VideoByTestIdAPIView.as_view(), name='video_by_test_id'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -3,10 +3,11 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from my_tests.models import TestUser
+from my_tests.models import TestUser, Test
 from users import permissions as p
 from . import serializers as s, models as m
 from .models import Video
+from .serializers import VideoSerializer
 
 
 class VideoCreateAPIView(generics.CreateAPIView):
@@ -47,3 +48,17 @@ class VideoDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = m.Video.objects.all()
     serializer_class = s.VideoSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class VideoByTestIdAPIView(generics.RetrieveAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+    def get(self, request, *args, **kwargs):
+        test_id = kwargs.get('test_id')
+        try:
+            test = Test.objects.get(pk=test_id)
+            video = test.video
+            serializer = self.get_serializer(video)
+            return Response(serializer.data)
+        except Test.DoesNotExist:
+            return Response({'error': 'Test not found'}, status=404)
